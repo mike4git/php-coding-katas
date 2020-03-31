38860b31e068d4254138f214b818282a406d0292
Vanessa Räbiger <v.raebiger@neusta.de>
2020-03-31 09:27:59 +0200

    Finalize ArabicToRomanConverter

--- src/ArabicToRomanConverter.php
+++ src/ArabicToRomanConverter.php
@@ -4,25 +4,29 @@ namespace Kata;
 
 class ArabicToRomanConverter
 {
-    public function convert(int $number): String
+    public function convert(int $number): string
     {
-        $roman = '';
-
-        $value = [
-            1 => 'I',
-            4 => 'IV',
-            5 => 'V',
+        $romanNumArray = [
+            'M' => 1000,
+            'CM' => 900,
+            'D' => 500,
+            'CD' => 400,
+            'C' => 100,
+            'XC' => 90,
+            'L' => 50,
+            'XL' => 40,
+            'X' => 10,
+            'IX' => 9,
+            'V' => 5,
+            'IV' => 4,
+            'I' => 1
         ];
 
+        $roman = '';
 
-        if (isset($value[$number])) {
-            return $value[$number];
-        }
-        if ((int)($number / 5) > 0) {
-            $roman .= 'V';
-        }
-        for ($i = 1; $i <= ($number % 5); $i++) {
-            $roman .= 'I';
+        foreach ($romanNumArray as $glyph => $num) {
+            $roman .= str_repeat($glyph, (int)($number/$num));
+            $number %= $num;
         }
 
         return $roman;
--- tests/ArabicToRomanConverterTest.php
+++ tests/ArabicToRomanConverterTest.php
@@ -10,6 +10,17 @@ class ArabicToRomanConverterTest extends TestCase
      */
     private $converter;
 
+    public function converterDataProvider(): iterable
+    {
+        yield 'convert 1' => ['I', 1];
+        yield 'convert 2' => ['II', 2];
+        yield 'convert 4' => ['IV', 4];
+        yield 'convert 5' => ['V', 5];
+        yield 'convert 7' => ['VII', 7];
+        yield 'convert 23' => ['XXIII', 23];
+        yield 'convert 1456' => ['MCDLVI', 1456];
+    }
+
     protected function setUp(): void
     {
         $this->converter = new ArabicToRomanConverter();
@@ -18,20 +29,11 @@ class ArabicToRomanConverterTest extends TestCase
     /**
      * @test
      * @dataProvider converterDataProvider
+     * @param string $expected
+     * @param int    $number
      */
-    public function convertOnInput(string $expected, int $number)
+    public function convertOnInput(string $expected, int $number): void
     {
         self::assertSame($expected, $this->converter->convert($number));
-    }
-
-    public function converterDataProvider(): iterable
-    {
-        yield ['', 0];
-        yield ['I', 1];
-        yield ['II', 2];
-        yield ['IV', 4];
-        yield ['V', 5];
-        yield ['VII', 7];
-        yield ['XXIII', 23];
     }
 }
