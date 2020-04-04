f302468f875b700912dc92d15bd6e159ab16c9b0
malbrecht <m.albrecht@neusta.de>
2020-04-04 09:05:00 +0200

    Happy numbers are all around the world...

--- src/HappyNumber.php
+++ src/HappyNumber.php
@@ -6,23 +6,65 @@ namespace Kata;
 
 class HappyNumber
 {
-    private $alreadyCheckedNumbers = [];
+    private $numbers = [];
+    private $checkedNumbers = [];
 
-    public function isHappy(int $number): bool
+    private const HAPPY = true;
+    private const UNHAPPY = false;
+
+    public function __construct()
     {
-        if ($number === 1) {
-            return true;
-        }
-        if (in_array($number, $this->alreadyCheckedNumbers)) {
-            return false;
-        }
-        $digit = 1;
-        $rest = $number;
-        $this->alreadyCheckedNumbers[] = $number;
-        $sumOfSquares = $this->sumUpSquaresOfDigits($rest);
-        return $this->isHappy($sumOfSquares);
+        $this->numbers[self::HAPPY]   = [1]; // by definition one is happy :-)
+        $this->numbers[self::UNHAPPY] = [];
     }
 
+    public function isHappy(int $number, bool $firstCall = true): bool
+    {
+        if ($this->isHappyNumber($number)) {
+            return $this->handleNumbers(self::HAPPY);
+        }
+        if ($this->isCheckedNumber($number)) {
+            return $this->handleNumbers(self::UNHAPPY);
+        }
+
+        $this->rememberAlreadyCheckedNumbers($number, $firstCall);
+
+        $sumOfSquares = $this->sumUpSquaresOfDigits($number);
+
+        return $this->isHappy($sumOfSquares, false);
+    }
+
+    /**
+     * @return bool
+     */
+    private function handleNumbers(bool $happy): bool
+    {
+        foreach ($this->checkedNumbers as $num) {
+            if (!in_array($num, $this->numbers[$happy])) {
+                $this->numbers[$happy][] = $num;
+            }
+        }
+        $this->checkedNumbers = [];
+        return $happy;
+    }
+
+    /**
+     * @param int $number
+     * @return bool
+     */
+    private function isHappyNumber(int $number): bool
+    {
+        return in_array($number, $this->numbers[true]);
+    }
+
+    /**
+     * @param int $number
+     * @return bool
+     */
+    private function isCheckedNumber(int $number): bool
+    {
+        return in_array($number, $this->checkedNumbers);
+    }
     /**
      * @param int $rest
      * @return float|int
@@ -37,4 +79,17 @@ class HappyNumber
         }
         return $sumOfSquares;
     }
+
+    /**
+     * @param int $number
+     * @param bool $firstCall
+     */
+    private function rememberAlreadyCheckedNumbers(int $number, bool $firstCall): void
+    {
+        if ($firstCall) {
+            $this->checkedNumbers = [];
+        }
+        $this->checkedNumbers[] = $number;
+    }
+
 }
--- tests/HappyNumberTest.php
+++ tests/HappyNumberTest.php
@@ -6,31 +6,53 @@ use PHPUnit\Framework\TestCase;
 
 class HappyNumberTest extends TestCase
 {
+    private $checker;
 
-    /**
-     * @test
-     */
-    public function isHappy1(): void
+    public function happyNumbersData()
     {
-        $checker = new HappyNumber();
-        self::assertTrue($checker->isHappy(1));
+        yield [1, true];
+        yield [2, false];
+        yield [3, false];
+        yield [7, true];
+        yield [10, true];
+        yield [19, true];
+        yield [22, false];
+    }
+
+
+    protected function setUp(): void
+    {
+        $this->checker = new HappyNumber();
+        parent::setUp();
     }
 
     /**
      * @test
+     * @dataProvider happyNumbersData
      */
-    public function isHappy19(): void
+    public function isHappy(int $number, bool $isHappy): void
     {
-        $checker = new HappyNumber();
-        self::assertTrue($checker->isHappy(19));
+        self::assertSame($isHappy, $this->checker->isHappy($number));
     }
 
     /**
+     * All happy numbers
+     * from https://de.wikipedia.org/wiki/Fr%C3%B6hliche_Zahl
+     * until 1000
      * @test
      */
-    public function isUnHappy2(): void
+    public function countOfHappyNumbersUntil1000(): void
     {
-        $checker = new HappyNumber();
-        self::assertFalse($checker->isHappy(2));
+        $maximumNumber = 1000;
+        $expectedCount = 143;
+        $actualCount = 0;
+
+        for ($number = 1; $number <= $maximumNumber; $number++) {
+            if ($this->checker->isHappy($number)) {
+                $actualCount++;
+            }
+        }
+        self::assertSame($expectedCount, $actualCount);
     }
+
 }
